6655f05a3360c838b92303380db943403222e6b9
Mojtaba Rahimy <mojtaba.rahimy@chmail.ir>
2020-04-09 22:15:48 +0430

    embedding delivery provider

--- app/src/main/java/ir/mrahimy/cafebazaar/data/dataclass/Delivery.kt
+++ app/src/main/java/ir/mrahimy/cafebazaar/data/dataclass/Delivery.kt
@@ -1,11 +1,13 @@
 package ir.mrahimy.cafebazaar.data.dataclass
 
 
+import androidx.room.Embedded
 import com.google.gson.annotations.SerializedName
 
 data class Delivery(
     @SerializedName("id")
     val id: String,
+    @Embedded(prefix = "provider")
     @SerializedName("provider")
     val provider: DeliveryProvider,
     @SerializedName("url")
