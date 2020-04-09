b3b6cec331c3ae4f086e0b73c1b09d21da9ff816
Mojtaba Rahimy <mojtaba.rahimy@chmail.ir>
2020-04-09 12:49:57 +0430

    +docs for api result encapsulation

--- app/src/main/java/ir/mrahimy/cafebazaar/network/reponse/FourSquareExploreVenuesRes.kt
+++ app/src/main/java/ir/mrahimy/cafebazaar/network/reponse/FourSquareExploreVenuesRes.kt
@@ -3,6 +3,11 @@ package ir.mrahimy.cafebazaar.network.reponse
 
 import com.google.gson.annotations.SerializedName
 
+/**
+ * This data class holds other responses.
+ * e.g. [PlacesApi][ir.mrahimy.cafebazaar.network.api.PlacesApi] returns
+ * [VenueQueryResponse] and not this class.
+ */
 data class FourSquareExploreVenuesRes(
     @SerializedName("meta")
     val meta: FourSquareResponseMetaData,
