940bdb2a7d8325c100b4277d338e74362c6e3729
Mojtaba Rahimy <mojtaba.rahimy@chmail.ir>
2020-04-09 22:57:03 +0430

    adding type converter for int list

---
+++ app/src/main/java/ir/mrahimy/cafebazaar/db/typeconverter/IntListConverter.kt
@@ -0,0 +1,23 @@
+package ir.mrahimy.cafebazaar.db.typeconverter
+
+import androidx.room.TypeConverter
+import com.google.gson.Gson
+import com.google.gson.reflect.TypeToken
+import java.io.Serializable
+
+
+class IntListConverter : Serializable {
+
+    private var gson = Gson()
+    @TypeConverter
+    fun fromList(list: List<Int>?): String {
+        val type = object : TypeToken<List<Int?>?>() {}.type
+        return gson.toJson(list, type)
+    }
+
+    @TypeConverter
+    fun fromDb(json: String): List<Int> {
+        val type = object : TypeToken<List<Int?>?>() {}.type
+        return gson.fromJson(json, type)
+    }
+}
