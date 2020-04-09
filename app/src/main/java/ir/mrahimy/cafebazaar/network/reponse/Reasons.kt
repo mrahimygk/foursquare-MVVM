7fc32e8dd7809f31948ebd273ba46cae1ad1f713
Mojtaba Rahimy <mojtaba.rahimy@chmail.ir>
2020-04-09 12:29:51 +0430

    +encapsulated reason list

---
+++ app/src/main/java/ir/mrahimy/cafebazaar/network/reponse/Reasons.kt
@@ -0,0 +1,12 @@
+package ir.mrahimy.cafebazaar.network.reponse
+
+
+import com.google.gson.annotations.SerializedName
+import ir.mrahimy.cafebazaar.data.dataclass.ReasonItem
+
+data class Reasons(
+    @SerializedName("count")
+    val count: Int,
+    @SerializedName("items")
+    val items: List<ReasonItem>
+)
