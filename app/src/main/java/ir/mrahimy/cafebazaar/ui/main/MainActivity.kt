3034604286fe77f45f3d65c2509454d6440c883a
Mojtaba Rahimy <mojtaba.rahimy@chmail.ir>
2020-04-09 22:14:41 +0430

    injecting vmodel and removing TODOs

--- app/src/main/java/ir/mrahimy/cafebazaar/ui/main/MainActivity.kt
+++ app/src/main/java/ir/mrahimy/cafebazaar/ui/main/MainActivity.kt
@@ -3,24 +3,21 @@ package ir.mrahimy.cafebazaar.ui.main
 import ir.mrahimy.cafebazaar.R
 import ir.mrahimy.cafebazaar.base.BaseActivity
 import ir.mrahimy.cafebazaar.databinding.ActivityMainBinding
+import org.koin.androidx.viewmodel.ext.android.viewModel
 
 class MainActivity : BaseActivity<MainViewModel, ActivityMainBinding>() {
 
     override val layoutRes = R.layout.activity_main
 
-    override val vm: MainViewModel
-        get() = TODO("not implemented") //To change initializer of created properties use File | Settings | File Templates.
+    override val vm: MainViewModel by viewModel()
 
     override fun bindObservables() {
-        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
     }
 
     override fun configCreationEvents() {
-        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
     }
 
     override fun configResumeEvents() {
-        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
     }
 
     override fun initBinding() {
