74c5666c2302ea726952a68371840ae90543bea6
Mojtaba Rahimy <mojtaba.rahimy@chmail.ir>
2020-04-09 12:20:18 +0430

    encapsulating location information as data class

---
+++ app/src/main/java/ir/mrahimy/cafebazaar/data/dataclass/Location.kt
@@ -0,0 +1,32 @@
+package ir.mrahimy.cafebazaar.data.dataclass
+
+
+import com.google.gson.annotations.SerializedName
+import ir.mrahimy.cafebazaar.data.dataclass.LabeledLatLng
+
+data class Location(
+    @SerializedName("address")
+    val address: String,
+    @SerializedName("cc")
+    val cc: String,
+    @SerializedName("city")
+    val city: String,
+    @SerializedName("country")
+    val country: String,
+    @SerializedName("crossStreet")
+    val crossStreet: String,
+    @SerializedName("distance")
+    val distance: Int,
+    @SerializedName("formattedAddress")
+    val formattedAddress: List<String>,
+    @SerializedName("labeledLatLngs")
+    val labeledLatLngList: List<LabeledLatLng>,
+    @SerializedName("lat")
+    val lat: Double,
+    @SerializedName("lng")
+    val lng: Double,
+    @SerializedName("postalCode")
+    val postalCode: String,
+    @SerializedName("state")
+    val state: String
+)
