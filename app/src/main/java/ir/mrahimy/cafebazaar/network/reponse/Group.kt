41c94684d9316bece130036bbe9699dd51f712ad
Mojtaba Rahimy <mojtaba.rahimy@chmail.ir>
2020-04-09 12:32:24 +0430

    + class to hold the searched venue category. e.g. recommended places.

---
+++ app/src/main/java/ir/mrahimy/cafebazaar/network/reponse/Group.kt
@@ -0,0 +1,16 @@
+package ir.mrahimy.cafebazaar.network.reponse
+
+
+import com.google.gson.annotations.SerializedName
+
+/**
+ * holds the searched venue group type, e.g. recommended places.
+ */
+data class Group(
+    @SerializedName("items")
+    val items: List<VenueItem>,
+    @SerializedName("name")
+    val name: String,
+    @SerializedName("type")
+    val type: String
+)
