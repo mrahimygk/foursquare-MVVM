02f9be7dba8086b48cc991387b0b92045da806c6
Mojtaba Rahimy <mojtaba.rahimy@chmail.ir>
2020-04-09 22:24:02 +0430

    adding a type converter for a list of nothing

---
+++ app/src/main/java/ir/mrahimy/cafebazaar/db/typeconverter/NothingListConverter.kt
@@ -0,0 +1,17 @@
+package ir.mrahimy.cafebazaar.db.typeconverter
+
+import androidx.room.TypeConverter
+import java.io.Serializable
+
+class NothingListConverter : Serializable {
+
+    @TypeConverter
+    fun fromList(list: List<Nothing>?): String {
+        return ""
+    }
+
+    @TypeConverter
+    fun fromDb(json: String): List<Nothing> {
+        return emptyList()
+    }
+}
