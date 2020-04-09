30d8847ced3571822615749a29d5dfdd905a49b3
Mojtaba Rahimy <mojtaba.rahimy@chmail.ir>
2020-04-09 21:44:50 +0430

    adding application class to init koin service

---
+++ app/src/main/java/ir/mrahimy/cafebazaar/application/FourSquareApplication.kt
@@ -0,0 +1,20 @@
+package ir.mrahimy.cafebazaar.application
+
+import android.app.Application
+import ir.mrahimy.cafebazaar.di.adapterModule
+import org.koin.android.ext.koin.androidContext
+import org.koin.android.ext.koin.androidLogger
+import org.koin.core.context.startKoin
+import org.koin.core.logger.Level
+
+class FourSquareApplication : Application() {
+
+    override fun onCreate() {
+        super.onCreate()
+        startKoin {
+            androidContext(this@FourSquareApplication)
+            androidLogger(Level.DEBUG)
+            modules(adapterModule)
+        }
+    }
+}
