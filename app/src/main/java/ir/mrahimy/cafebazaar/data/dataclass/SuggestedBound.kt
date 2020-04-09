8b62362ca2095f8161f61362cee3f1c55af65778
Mojtaba Rahimy <mojtaba.rahimy@chmail.ir>
2020-04-09 12:16:42 +0430

    encapsulating lat&long as data class

---
+++ app/src/main/java/ir/mrahimy/cafebazaar/data/dataclass/SuggestedBound.kt
@@ -0,0 +1,11 @@
+package ir.mrahimy.cafebazaar.data.dataclass
+
+
+import com.google.gson.annotations.SerializedName
+
+data class SuggestedBound(
+    @SerializedName("lat")
+    val lat: Double,
+    @SerializedName("lng")
+    val lng: Double
+)
