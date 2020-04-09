5f505db6bf7b543cb8c3a9389f9c77ff8fe55426
Mojtaba Rahimy <mojtaba.rahimy@chmail.ir>
2020-04-09 12:50:21 +0430

    replacing api result with the actual venue response

--- app/src/main/java/ir/mrahimy/cafebazaar/network/api/PlacesApi.kt
+++ app/src/main/java/ir/mrahimy/cafebazaar/network/api/PlacesApi.kt
@@ -1,10 +1,11 @@
 package ir.mrahimy.cafebazaar.network.api
 
+import ir.mrahimy.cafebazaar.network.reponse.VenueQueryResponse
 import retrofit2.http.GET
 import retrofit2.http.QueryMap
 
 interface PlacesApi {
 
-    @GET("/explore")
-    suspend fun getPlaces(@QueryMap params: Map<String, String>): Any
+    @GET("venues/explore/")
+    suspend fun getPlaces(@QueryMap params: Map<String, String>): VenueQueryResponse
 }
