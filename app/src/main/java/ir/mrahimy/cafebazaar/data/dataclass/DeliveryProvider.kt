1997dded5e03dd1f6057113df2f272a0c626321f
Mojtaba Rahimy <mojtaba.rahimy@chmail.ir>
2020-04-09 22:15:30 +0430

    embedding address

--- app/src/main/java/ir/mrahimy/cafebazaar/data/dataclass/DeliveryProvider.kt
+++ app/src/main/java/ir/mrahimy/cafebazaar/data/dataclass/DeliveryProvider.kt
@@ -1,9 +1,11 @@
 package ir.mrahimy.cafebazaar.data.dataclass
 
 
+import androidx.room.Embedded
 import com.google.gson.annotations.SerializedName
 
 data class DeliveryProvider(
+    @Embedded(prefix = "icon")
     @SerializedName("icon")
     val icon: NamedIconAddress,
     @SerializedName("name")
