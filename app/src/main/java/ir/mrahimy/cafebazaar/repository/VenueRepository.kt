5515d5cd650d64d93fe2c642e9aab8dffab27723
Mojtaba Rahimy <mojtaba.rahimy@chmail.ir>
2020-04-10 10:43:46 +0430

    adding a fun to insert single venue into db

--- app/src/main/java/ir/mrahimy/cafebazaar/repository/VenueRepository.kt
+++ app/src/main/java/ir/mrahimy/cafebazaar/repository/VenueRepository.kt
@@ -1,9 +1,11 @@
 package ir.mrahimy.cafebazaar.repository
 
+import androidx.lifecycle.LiveData
 import ir.mrahimy.cafebazaar.data.dataclass.Venue
 
 interface VenueRepository {
     suspend fun get(): List<Venue>
-    suspend fun getOffline(): List<Venue>
+    fun getOffline(): LiveData<List<Venue>>
     suspend fun sync()
+    suspend fun insert(venue: Venue)
 }
