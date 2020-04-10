999ce62d11aab5824e8c667982178b4dc628f16d
Mojtaba Rahimy <mojtaba.rahimy@chmail.ir>
2020-04-10 08:40:49 +0430

    adding retrofit qualifier to koin network module

--- app/src/main/java/ir/mrahimy/cafebazaar/di/NetworkModule.kt
+++ app/src/main/java/ir/mrahimy/cafebazaar/di/NetworkModule.kt
@@ -1,14 +1,18 @@
 package ir.mrahimy.cafebazaar.di
 
+import ir.mrahimy.cafebazaar.network.BaseUrl
 import okhttp3.Interceptor
 import okhttp3.OkHttpClient
 import okhttp3.logging.HttpLoggingInterceptor
 import org.koin.core.qualifier.Qualifier
 import org.koin.dsl.module
+import retrofit2.Retrofit
+import retrofit2.converter.gson.GsonConverterFactory
 import timber.log.Timber
 
 object LoggingInterceptorQualifier : Qualifier
 object OkHttpQualifier : Qualifier
+object RetrofitQualifier : Qualifier
 
 val networkModule = module {
 
@@ -28,4 +32,11 @@ val networkModule = module {
         }.build()
     }
 
+    single<Retrofit>(RetrofitQualifier) {
+        Retrofit.Builder()
+            .baseUrl(BaseUrl.BASE_URL)
+            .client(get(OkHttpQualifier))
+            .addConverterFactory(GsonConverterFactory.create(get()))
+            .build()
+    }
 }
