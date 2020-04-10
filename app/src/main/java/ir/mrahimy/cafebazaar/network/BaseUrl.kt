4cc86f33876de7b867dfaf0c848c54f897e47132
Mojtaba Rahimy <mojtaba.rahimy@chmail.ir>
2020-04-10 08:40:00 +0430

    removing sub route from base url

--- app/src/main/java/ir/mrahimy/cafebazaar/network/BaseUrl.kt
+++ app/src/main/java/ir/mrahimy/cafebazaar/network/BaseUrl.kt
@@ -3,6 +3,6 @@ package ir.mrahimy.cafebazaar.network
 object BaseUrl {
     private const val VERSION = 2
 
-    const val BASE_URL = "https://api.foursquare.com/v$VERSION/venues"
+    const val BASE_URL = "https://api.foursquare.com/v$VERSION/"
 
 }
