329782fc05d1ea375bb452d6768b0c0ee2fb8d82
Mojtaba Rahimy <mojtaba.rahimy@chmail.ir>
2020-04-10 16:42:29 +0430

    another re-package

--- app/src/main/java/ir/mrahimy/cafebazaar/db/typeconverter/StringListConverter.kt
+++ app/src/main/java/ir/mrahimy/cafebazaar/db/typeconverter/StringListConverter.kt
@@ -1,8 +1,8 @@
 package ir.mrahimy.cafebazaar.db.typeconverter
 
 import androidx.room.TypeConverter
-import androidx.room.TypeConverters
-import ir.mrahimy.cafebazaar.data.dataclass.VenueCategory
+import ir.mrahimy.cafebazaar.ktx.serialization.fromJson
+import ir.mrahimy.cafebazaar.ktx.serialization.toJson
 import java.io.Serializable
 
 class StringListConverter : Serializable {
