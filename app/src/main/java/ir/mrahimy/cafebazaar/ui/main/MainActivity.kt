9e4f13320abc1015f38d805df779c900d0dee0d7
Mojtaba Rahimy <mojtaba.rahimy@chmail.ir>
2020-04-10 10:16:59 +0430

    using venue adapter and testing shown data in paged recycler view

--- app/src/main/java/ir/mrahimy/cafebazaar/ui/main/MainActivity.kt
+++ app/src/main/java/ir/mrahimy/cafebazaar/ui/main/MainActivity.kt
@@ -1,8 +1,13 @@
 package ir.mrahimy.cafebazaar.ui.main
 
+import androidx.lifecycle.lifecycleScope
 import ir.mrahimy.cafebazaar.R
 import ir.mrahimy.cafebazaar.base.BaseActivity
 import ir.mrahimy.cafebazaar.databinding.ActivityMainBinding
+import ir.mrahimy.cafebazaar.util.PagedRecyclerViewScrollListener
+import kotlinx.android.synthetic.main.activity_main.*
+import kotlinx.coroutines.launch
+import org.koin.android.ext.android.inject
 import org.koin.androidx.viewmodel.ext.android.viewModel
 
 class MainActivity : BaseActivity<MainViewModel, ActivityMainBinding>() {
@@ -11,10 +16,21 @@ class MainActivity : BaseActivity<MainViewModel, ActivityMainBinding>() {
 
     override val vm: MainViewModel by viewModel()
 
+    private val venuesAdapter: VenuesAdapter by inject()
     override fun bindObservables() {
     }
 
     override fun configCreationEvents() {
+        venueList?.run {
+            adapter = venuesAdapter
+            layoutManager?.let {
+                addOnScrollListener(object : PagedRecyclerViewScrollListener(it) {
+                    override fun onLoadMore(page: Int) {
+                        lifecycleScope.launch { vm.initList() }
+                    }
+                })
+            }
+        }
     }
 
     override fun configResumeEvents() {
