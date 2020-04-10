a1dbebaccfe51fa302ddb9eec06a4e2ad41974cb
Mojtaba Rahimy <mojtaba.rahimy@chmail.ir>
2020-04-10 16:45:06 +0430

    sync needs query map in request data

--- app/src/main/java/ir/mrahimy/cafebazaar/repository/VenueRepository.kt
+++ app/src/main/java/ir/mrahimy/cafebazaar/repository/VenueRepository.kt
@@ -7,6 +7,6 @@ import ir.mrahimy.cafebazaar.network.ApiResult
 interface VenueRepository {
     suspend fun get(): List<Venue>
     fun getOffline(): LiveData<List<Venue>>
-    suspend fun sync(): ApiResult<Any>
+    suspend fun sync(queryMap: Map<String, String>): ApiResult<Any>
     suspend fun insert(venue: Venue)
 }
