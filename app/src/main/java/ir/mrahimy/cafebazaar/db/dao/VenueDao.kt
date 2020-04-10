47383fe63ef715a0449107cd88ca364141683816
Mojtaba Rahimy <mojtaba.rahimy@chmail.ir>
2020-04-10 18:44:24 +0430

    +new fun to get venue by id

--- app/src/main/java/ir/mrahimy/cafebazaar/db/dao/VenueDao.kt
+++ app/src/main/java/ir/mrahimy/cafebazaar/db/dao/VenueDao.kt
@@ -15,4 +15,7 @@ interface VenueDao : BaseDao<Venue> {
     @Query("SELECT * FROM venue WHERE id in (:ids)")
     fun get(vararg ids: String): LiveData<List<Venue>>
 
+    @Query("SELECT * FROM venue WHERE id =:id LIMIT 1")
+    suspend fun getSuspend(id: String): Venue
+
 }
