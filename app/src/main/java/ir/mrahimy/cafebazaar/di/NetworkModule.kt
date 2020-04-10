d1b06028791b32122de1a61df552b49fb8cf2146
Mojtaba Rahimy <mojtaba.rahimy@chmail.ir>
2020-04-10 09:30:09 +0430

    adding cache to network module

--- app/src/main/java/ir/mrahimy/cafebazaar/di/NetworkModule.kt
+++ app/src/main/java/ir/mrahimy/cafebazaar/di/NetworkModule.kt
@@ -1,21 +1,31 @@
 package ir.mrahimy.cafebazaar.di
 
 import ir.mrahimy.cafebazaar.network.BaseUrl
+import okhttp3.Cache
 import okhttp3.Interceptor
 import okhttp3.OkHttpClient
 import okhttp3.logging.HttpLoggingInterceptor
+import org.koin.android.ext.koin.androidContext
 import org.koin.core.qualifier.Qualifier
 import org.koin.dsl.module
 import retrofit2.Retrofit
 import retrofit2.converter.gson.GsonConverterFactory
 import timber.log.Timber
+import java.io.File
 
 object LoggingInterceptorQualifier : Qualifier
 object OkHttpQualifier : Qualifier
+object OkHttpCacheQualifier : Qualifier
 object RetrofitQualifier : Qualifier
 
 val networkModule = module {
 
+    factory(OkHttpCacheQualifier) {
+        File(androidContext().cacheDir, "cache").apply {
+            if (!exists()) mkdir()
+        }
+    }
+    factory { Cache(get(OkHttpCacheQualifier), 2L * 1024L * 1024L) }
 
     factory<Interceptor>(LoggingInterceptorQualifier) {
         HttpLoggingInterceptor { log ->
