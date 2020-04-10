bfee4d6ef6247949b9c929f4bc6fecfc72cf5e69
Mojtaba Rahimy <mojtaba.rahimy@chmail.ir>
2020-04-10 16:44:27 +0430

    +4sq api version

--- app/src/main/java/ir/mrahimy/cafebazaar/network/BaseUrl.kt
+++ app/src/main/java/ir/mrahimy/cafebazaar/network/BaseUrl.kt
@@ -2,6 +2,7 @@ package ir.mrahimy.cafebazaar.network
 
 object BaseUrl {
     private const val VERSION = 2
+    const val FOURSQUARE_VERSION = "20200509"
 
     const val BASE_URL = "https://api.foursquare.com/v$VERSION/"
 
