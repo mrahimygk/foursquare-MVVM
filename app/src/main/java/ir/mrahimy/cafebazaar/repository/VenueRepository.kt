4286b9de8c0f3e03c4f8aa6fe439ebabba4fe627
Mojtaba Rahimy <mojtaba.rahimy@chmail.ir>
2020-04-10 11:20:09 +0430

    getting api result to decide on retry

--- app/src/main/java/ir/mrahimy/cafebazaar/repository/VenueRepository.kt
+++ app/src/main/java/ir/mrahimy/cafebazaar/repository/VenueRepository.kt
@@ -2,10 +2,11 @@ package ir.mrahimy.cafebazaar.repository
 
 import androidx.lifecycle.LiveData
 import ir.mrahimy.cafebazaar.data.dataclass.Venue
+import ir.mrahimy.cafebazaar.network.ApiResult
 
 interface VenueRepository {
     suspend fun get(): List<Venue>
     fun getOffline(): LiveData<List<Venue>>
-    suspend fun sync()
+    suspend fun sync(): ApiResult<Any>
     suspend fun insert(venue: Venue)
 }
