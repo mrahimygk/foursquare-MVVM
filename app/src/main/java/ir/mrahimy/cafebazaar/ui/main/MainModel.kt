8357281a3dd09225179b0411c65752c400d18048
Mojtaba Rahimy <mojtaba.rahimy@chmail.ir>
2020-04-10 16:47:06 +0430

    removing mock data and adding sync by venueApi

--- app/src/main/java/ir/mrahimy/cafebazaar/ui/main/MainModel.kt
+++ app/src/main/java/ir/mrahimy/cafebazaar/ui/main/MainModel.kt
@@ -10,39 +10,18 @@ import kotlin.random.Random
 class MainModel(
     private val venueRepository: VenueRepository
 ) : BaseModel() {
+
     suspend fun get() = venueRepository.get()
+
     val offlineVenueList = venueRepository.getOffline()
-    private fun makeMockVenue(i: Int): Venue =
-        Venue(
-            listOf(),
-            Delivery(
-                i.toString(),
-                DeliveryProvider(NamedIconAddress("a", "b", listOf()), "name"),
-                "http"
-            ),
-            "id$i", Location(
-                "add",
-                "ct$i",
-                "city$i",
-                "iran",
-                "onja",
-                100,
-                listOf("aaa$i", "bbb$i"),
-                listOf(
-                    LabeledLatLng("inja", 100.0, 200.0)
-                ),
-                200.0,
-                150.0,
-                "9874$i",
-                "CA"
-            ), "CAffeLate", Photos(1, listOf()),
-            VenuePage("2$i")
+
+    suspend fun syncVenueList(offset: Int/*TODO: GET raw LOCATION DATA FROM vm <-- activity*/) =
+        venueRepository.sync(
+            mapOf(
+                "ll" to "40.7243,-74.0018",
+                "limit" to "2",
+                "offset" to offset.toString()
+            )
         )
-
-    suspend fun addMockVenue() {
-        venueRepository.insert(makeMockVenue(Random.nextInt(10000)))
-    }
-
-    suspend fun sync() = venueRepository.sync()
 
 }
