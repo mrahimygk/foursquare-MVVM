bbb97332ce13c8b1ef5bf11815d659cb5114227a
Mojtaba Rahimy <mojtaba.rahimy@chmail.ir>
2020-04-10 18:49:58 +0430

    adding details vm to injections

--- app/src/main/java/ir/mrahimy/cafebazaar/di/ViewModelModule.kt
+++ app/src/main/java/ir/mrahimy/cafebazaar/di/ViewModelModule.kt
@@ -1,9 +1,11 @@
 package ir.mrahimy.cafebazaar.di
 
+import ir.mrahimy.cafebazaar.ui.details.DetailsViewModel
 import ir.mrahimy.cafebazaar.ui.main.MainViewModel
 import org.koin.androidx.viewmodel.dsl.viewModel
 import org.koin.dsl.module
 
 val viewModelModule = module {
     viewModel { MainViewModel(get()) }
+    viewModel { DetailsViewModel(get()) }
 }
