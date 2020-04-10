bcfe4494ee6725707beedeba265faa0f25f96a56
Mojtaba Rahimy <mojtaba.rahimy@chmail.ir>
2020-04-10 10:04:07 +0430

    adding binding adapters  for view visibility

---
+++ app/src/main/java/ir/mrahimy/cafebazaar/ba/View.kt
@@ -0,0 +1,20 @@
+package ir.mrahimy.cafebazaar.ba
+
+import android.view.View
+import androidx.databinding.BindingAdapter
+
+@BindingAdapter("app:isVisible")
+fun View.setIsVisible(boolean: Boolean?) {
+    boolean?.let {
+        visibility = if (it) View.VISIBLE
+        else View.INVISIBLE
+    }
+}
+
+@BindingAdapter("app:isGone")
+fun View.setIsGone(boolean: Boolean?) {
+    boolean?.let {
+        visibility = if (it) View.GONE
+        else View.VISIBLE
+    }
+}
