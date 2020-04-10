e1f74d10a2559f030707f9a022e2d38a3ac66611
Mojtaba Rahimy <mojtaba.rahimy@chmail.ir>
2020-04-10 10:08:33 +0430

    adding a fun to init venue list to test paged recycler view

--- app/src/main/java/ir/mrahimy/cafebazaar/ui/main/MainViewModel.kt
+++ app/src/main/java/ir/mrahimy/cafebazaar/ui/main/MainViewModel.kt
@@ -4,24 +4,38 @@ import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.viewModelScope
 import ir.mrahimy.cafebazaar.base.BaseViewModel
+import ir.mrahimy.cafebazaar.data.dataclass.*
+import ir.mrahimy.cafebazaar.network.reponse.Photos
+import ir.mrahimy.cafebazaar.network.reponse.VenuePage
 import kotlinx.coroutines.delay
 import kotlinx.coroutines.launch
 
 class MainViewModel(private val model: MainModel) : BaseViewModel(model) {
 
-    private val _test = MutableLiveData<String>("")
-    val test: LiveData<String>
-        get() = _test
+    private val _venueList = MutableLiveData<List<Venue>>()
+    val venueList: LiveData<List<Venue>>
+        get() = _venueList
+
+    private val _isLoading = MutableLiveData<Boolean>(false)
+    val isLoading: LiveData<Boolean>
+        get() = _isLoading
 
     init {
         viewModelScope.launch {
-            var i = 0
-            while (true) {
-                delay(100)
-                _test.postValue(i.toString())
-                i++
-            }
-
+            initList()
         }
+    }
+
+    suspend fun initList() {
+        _isLoading.postValue(true)
+        var i = 0
+        repeat(15) {
+            delay(100)
+            _venueList.postValue(mutableListOf<Venue>().apply {
+                addAll(venueList.value ?: listOf())
+                add(model.makeMockVenue(i++))
+            })
+        }
+        _isLoading.postValue(false)
     }
 }
