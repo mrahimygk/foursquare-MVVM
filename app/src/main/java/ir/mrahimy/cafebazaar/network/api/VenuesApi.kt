d557994d1586a1213c2671607ae7a60623ab835e
Mojtaba Rahimy <mojtaba.rahimy@chmail.ir>
2020-04-10 11:17:22 +0430

    adding hints from 4sq docs

--- app/src/main/java/ir/mrahimy/cafebazaar/network/api/VenuesApi.kt
+++ app/src/main/java/ir/mrahimy/cafebazaar/network/api/VenuesApi.kt
@@ -4,8 +4,17 @@ import ir.mrahimy.cafebazaar.network.reponse.VenueQueryResponse
 import retrofit2.http.GET
 import retrofit2.http.QueryMap
 
+/**
+ * 4sq DOCS:
+ * (ll and radius) or
+ * (sw and ne) or
+ * (near and radius) or
+ * (nearVenueId and ll) or
+ * (superVenueId) or
+ * (polygon)"
+ */
 interface VenuesApi {
 
-    @GET("venues/explore/")
+    @GET("venues/explore")
     suspend fun query(@QueryMap params: Map<String, String>): VenueQueryResponse
 }
