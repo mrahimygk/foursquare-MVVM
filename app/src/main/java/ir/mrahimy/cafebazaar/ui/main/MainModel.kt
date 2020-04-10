438f73f43bfdd16341741cf70cf9fa6b5088a2fd
Mojtaba Rahimy <mojtaba.rahimy@chmail.ir>
2020-04-10 10:07:58 +0430

    adding a test fun to make simple mocked venues

--- app/src/main/java/ir/mrahimy/cafebazaar/ui/main/MainModel.kt
+++ app/src/main/java/ir/mrahimy/cafebazaar/ui/main/MainModel.kt
@@ -1,10 +1,39 @@
 package ir.mrahimy.cafebazaar.ui.main
 
 import ir.mrahimy.cafebazaar.base.BaseModel
+import ir.mrahimy.cafebazaar.data.dataclass.*
+import ir.mrahimy.cafebazaar.network.reponse.Photos
+import ir.mrahimy.cafebazaar.network.reponse.VenuePage
 import ir.mrahimy.cafebazaar.repository.VenueRepository
 
 class MainModel(
     private val venueRepository: VenueRepository
 ) : BaseModel() {
     suspend fun get() = venueRepository.get()
+    fun makeMockVenue(i: Int): Venue =
+        Venue(
+            listOf(),
+            Delivery(
+                i.toString(),
+                DeliveryProvider(NamedIconAddress("a", "b", listOf()), "name"),
+                "http"
+            ),
+            "id$i", Location(
+                "add",
+                "ct$i",
+                "city$i",
+                "iran",
+                "onja",
+                100,
+                listOf("aaa$i", "bbb$i"),
+                listOf(
+                    LabeledLatLng("inja", 100.0, 200.0)
+                ),
+                200.0,
+                150.0,
+                "9874$i",
+                "CA"
+            ), "CAffeLate", Photos(1, listOf()),
+            VenuePage("2$i")
+        )
 }
