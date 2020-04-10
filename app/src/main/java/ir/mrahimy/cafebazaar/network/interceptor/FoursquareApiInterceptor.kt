c7a49fb77bf28bc7f358252a04fe6445261c4b05
Mojtaba Rahimy <mojtaba.rahimy@chmail.ir>
2020-04-10 11:17:01 +0430

    intercepting 4sq calls to always add auth tokens

---
+++ app/src/main/java/ir/mrahimy/cafebazaar/network/interceptor/FoursquareApiInterceptor.kt
@@ -0,0 +1,39 @@
+package ir.mrahimy.cafebazaar.network.interceptor
+
+import okhttp3.HttpUrl
+import okhttp3.Interceptor
+import okhttp3.Response
+
+
+class FoursquareApiInterceptor(val version: String = "20200101") : Interceptor {
+
+    override fun intercept(chain: Interceptor.Chain): Response {
+
+        var request = chain.request()
+        val url: HttpUrl = request.url().newBuilder()
+            .addQueryParameter(
+                "client_id",
+                "0RC3M3SBMKAQWRKQEUKRXT0C4UE5L2N0JD3V4C45DYS0APUT"
+            ).addQueryParameter(
+                "client_secret",
+                "NR5VX04CU0O1KGAEUG4I3DKJXS5V0YCUZACZ2MS1ULM4CO3E"
+            ).addQueryParameter(
+                "v",
+                version
+            )
+            .build()
+        request = request.newBuilder().url(url).build()
+        return chain.proceed(request)
+//        var request = chain.request().newBuilder()
+//            .addHeader(
+//                "client_id",
+//                "0RC3M3SBMKAQWRKQEUKRXT0C4UE5L2N0JD3V4C45DYS0APUT"
+//            )
+//            .addHeader(
+//                "client_secret",
+//                "NR5VX04CU0O1KGAEUG4I3DKJXS5V0YCUZACZ2MS1ULM4CO3E"
+//            ).build()
+//        return chain.proceed(request)
+    }
+
+}
