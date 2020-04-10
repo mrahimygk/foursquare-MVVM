511c0df695b99b2f74a467d8ef54ddbe73f0945c
Mojtaba Rahimy <mojtaba.rahimy@chmail.ir>
2020-04-10 10:16:14 +0430

    adding adapter module to koin service

--- app/src/main/java/ir/mrahimy/cafebazaar/di/AdapterModule.kt
+++ app/src/main/java/ir/mrahimy/cafebazaar/di/AdapterModule.kt
@@ -1,8 +1,10 @@
 package ir.mrahimy.cafebazaar.di
 
 import ir.mrahimy.cafebazaar.ui.main.MockAdapter
+import ir.mrahimy.cafebazaar.ui.main.VenuesAdapter
 import org.koin.dsl.module
 
 val adapterModule = module {
     factory { MockAdapter() }
+    factory { VenuesAdapter() }
 }
