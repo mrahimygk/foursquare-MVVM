45f479d8dac4ddc36b89772b97bae8e39b7e5f09
Mojtaba Rahimy <mojtaba.rahimy@chmail.ir>
2020-04-10 08:18:15 +0430

    adding tests for venue serialization

--- app/src/test/java/ir/mrahimy/cafebazaar/SerializationUnitTest.kt
+++ app/src/test/java/ir/mrahimy/cafebazaar/SerializationUnitTest.kt
@@ -1,8 +1,12 @@
 package ir.mrahimy.cafebazaar
 
 import ir.mrahimy.cafebazaar.data.dataclass.LabeledLatLng
+import ir.mrahimy.cafebazaar.data.dataclass.SuffixedIconAddress
+import ir.mrahimy.cafebazaar.data.dataclass.VenueCategory
 import ir.mrahimy.cafebazaar.db.typeconverter.IntListConverter
 import ir.mrahimy.cafebazaar.db.typeconverter.LabeledLatLngListConverter
+import ir.mrahimy.cafebazaar.db.typeconverter.StringListConverter
+import ir.mrahimy.cafebazaar.db.typeconverter.VenueCategoryListConverter
 import org.junit.Test
 
 import org.junit.Assert.*
@@ -56,5 +60,70 @@ class SerializationUnitTest {
 
         println(serializerTime)
         println(gsonTime)
+    }
+
+    @Test
+    fun `test converting string list to json and getting it back`() {
+        val list = listOf(
+            "Ali",
+            "Dali",
+            "Naghi",
+            "Mashti",
+            "Juju"
+        )
+
+        val slc = StringListConverter()
+        val jsonFromList = slc.fromList(list)
+        val listFromJson = slc.fromDb(jsonFromList)
+        val jsonFromDbList = slc.fromList(listFromJson)
+
+        println(list)
+        println(listFromJson)
+        println(jsonFromList)
+        println(jsonFromDbList)
+
+        assert(list == listFromJson)
+        assert(jsonFromDbList == jsonFromList)
+
+    }
+
+    @Test
+    fun `test converting venue category list to json and getting it back`() {
+        val list = listOf(
+            VenueCategory(
+                SuffixedIconAddress("a", ".png"),
+                "1",
+                "name1",
+                "names1",
+                true,
+                "shortname1"
+            ),
+            VenueCategory(
+                SuffixedIconAddress("b", ".jpg"),
+                "2",
+                "name2",
+                "names2",
+                true,
+                "shortname2"
+            ),
+            VenueCategory(
+                SuffixedIconAddress("c", ".gif"),
+                "3",
+                "name3",
+                "names3",
+                true,
+                "shortname3"
+            )
+        )
+
+        val vclc = VenueCategoryListConverter()
+        val jsonFromList = vclc.fromList(list)
+        val listFromJson = vclc.fromDb(jsonFromList)
+        val jsonFromDbList = vclc.fromList(listFromJson)
+
+        println(list)
+        println(listFromJson)
+        println(jsonFromList)
+        println(jsonFromDbList)
     }
 }
