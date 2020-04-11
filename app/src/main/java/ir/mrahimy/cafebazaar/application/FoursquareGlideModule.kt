05a5c38c02b9f5eec6abda97af7cd92ec51e69d0
Mojtaba Rahimy <mojtaba.rahimy@chmail.ir>
2020-04-11 11:27:16 +0430

    adding the required glide module

---
+++ app/src/main/java/ir/mrahimy/cafebazaar/application/FoursquareGlideModule.kt
@@ -0,0 +1,14 @@
+package ir.mrahimy.cafebazaar.application
+
+import android.content.Context
+import com.bumptech.glide.GlideBuilder
+import com.bumptech.glide.annotation.GlideModule
+import com.bumptech.glide.module.AppGlideModule
+
+@GlideModule
+class FoursquareGlideModule: AppGlideModule() {
+
+    override fun applyOptions(context: Context, builder: GlideBuilder) {
+        super.applyOptions(context, builder)
+    }
+}
