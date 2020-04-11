702cae571f0ddb4fc947668b9089c44ed0e9f8c3
Mojtaba Rahimy <mojtaba.rahimy@chmail.ir>
2020-04-11 13:34:50 +0430

    adding ktx on imageview to load image with glide

--- app/src/main/java/ir/mrahimy/cafebazaar/ktx/ImageView.kt
+++ app/src/main/java/ir/mrahimy/cafebazaar/ktx/ImageView.kt
@@ -1,13 +1,15 @@
 package ir.mrahimy.cafebazaar.ktx
 
+import android.graphics.drawable.Drawable
 import android.widget.ImageView
 import ir.mrahimy.cafebazaar.application.GlideApp
 
 fun ImageView.loadUrl(
-    url: String?
+    url: String?,
+    placeholder: Drawable? = null
 ) {
     GlideApp
-        .with(this)
+        .with(this.context)
         .load(url)
         .into(this)
 }
