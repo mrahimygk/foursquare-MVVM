bacd288fd9ddbc9464c5b67254fef9961cf6db88
Mojtaba Rahimy <mojtaba.rahimy@chmail.ir>
2020-04-11 11:04:25 +0430

    adding a field to show snack messages on all viewmodels

--- app/src/main/java/ir/mrahimy/cafebazaar/base/BaseViewModel.kt
+++ app/src/main/java/ir/mrahimy/cafebazaar/base/BaseViewModel.kt
@@ -1,5 +1,17 @@
 package ir.mrahimy.cafebazaar.base
 
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
+import ir.mrahimy.cafebazaar.helper.Event
 
-abstract class BaseViewModel(private val model: BaseModel) : ViewModel()
+abstract class BaseViewModel(private val model: BaseModel) : ViewModel() {
+
+    /**
+     * classes extending [BaseViewModel] can use _snackMessage to post string resources
+     * Views should observe on the public value [snackMessage]
+     */
+    protected val _snackMessage = MutableLiveData<Event<Int>>()
+    val snackMessage: LiveData<Event<Int>>
+        get() = _snackMessage
+}
