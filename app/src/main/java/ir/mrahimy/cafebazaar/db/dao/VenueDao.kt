7dab7882a7f39a214808f7d381931a9080a5d9be
Mojtaba Rahimy <mojtaba.rahimy@chmail.ir>
2020-04-11 10:51:28 +0430

    adding order to query. adding count for fetch order .

--- app/src/main/java/ir/mrahimy/cafebazaar/db/dao/VenueDao.kt
+++ app/src/main/java/ir/mrahimy/cafebazaar/db/dao/VenueDao.kt
@@ -9,13 +9,22 @@ import ir.mrahimy.cafebazaar.data.dataclass.Venue
 @Dao
 interface VenueDao : BaseDao<Venue> {
 
-    @Query("SELECT * FROM venue")
-    fun get(): LiveData<List<Venue>>
+    @Query("SELECT * FROM venue ORDER BY fetched_order")
+    fun getAll(): LiveData<List<Venue>>
 
-    @Query("SELECT * FROM venue WHERE id in (:ids)")
+    @Query("SELECT * FROM venue WHERE id in (:ids) ORDER BY fetched_order")
     fun get(vararg ids: String): LiveData<List<Venue>>
 
     @Query("SELECT * FROM venue WHERE id =:id LIMIT 1")
     suspend fun getSuspend(id: String): Venue
+
+    /**
+     * counting them to add the order index
+     */
+    @Query("SELECT COUNT(id) FROM venue")
+    suspend fun count(): Int
+
+    @Query("DELETE FROM venue WHERE 1")
+    suspend fun clearAll(): Int
 
 }
